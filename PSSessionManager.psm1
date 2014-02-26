9d56278966562f61d419c8e8378d331342ca283e
jrich523 <jrich523@gmail.com>
2014-02-26 13:36:56 -0800

    updated to support new files

--- PSSessionManager.psm1
+++ PSSessionManager.psm1
@@ -2,3 +2,5 @@ try{Add-Type -Path $PSScriptRoot\MonitorCommand.cs}catch{}
 
 . $PSScriptRoot\PSSMListener.ps1
 . $PSScriptRoot\Connect-PSSMSession.ps1
+. $PSScriptRoot\copy-item.ps1
+. $PSScriptRoot\find-credential.ps1
